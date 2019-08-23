935a8e94f6ca60362a90576b56ad2aea079473af
honorine1 <uwurukundoh@gmail.com>
2019-08-23 17:16:31 +0200

    form-component and its content is added

--- Quote/src/app/app.module.ts
+++ Quote/src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { VoteComponent } from './vote/vote.component';
+import { DateCountPipe } from './date-count.pipe';
+import { FormQuoteComponent } from './form-quote/form-quote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailsComponent,
-    VoteComponent
+    VoteComponent,
+    DateCountPipe,
+    FormQuoteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Quote/src/app/quote/quote.component.ts
+++ Quote/src/app/quote/quote.component.ts
@@ -11,6 +11,7 @@ export class QuoteComponent implements OnInit {
       new Quote (1,'The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself.',' Mark Cain',new Date(2019/9/1)),
       new Quote (2,'When one door of happiness closes, another opens; but often we look so long at the closed door that we do not see the one which has been opened for us.','Helen Keller',new Date(2019/9/1)),
       new Quote (3, 'Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do..','Mark Twain',new Date(2019/9/1)),
+      new Quote (4,'Happy people plan actions, they don’t plan results.','— Dennis Waitley',new Date(2019/12/1)),
     ];
 //     upvoteQuote(isUpvote, index){
 //       if(isUpvote){
