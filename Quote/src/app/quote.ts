8c1363b6b15685ab43be5f9c438d33f3e47d3645
honorine1 <uwurukundoh@gmail.com>
2019-08-25 14:12:23 +0200

    border bottom  is added

--- Quote/src/app/quote.ts
+++ Quote/src/app/quote.ts
@@ -1,7 +1,5 @@
 export class Quote {
-    // id:number;
-    // name:string;
-    // author:string;
+   
 showAuthor: boolean;
 constructor(public id:number,public pname: string, public quote: string,public author: string,public voteDate: Date){
     this.showAuthor=false;
