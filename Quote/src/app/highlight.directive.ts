8879b90756de8b0dbb4cba8f86aeb548c53b28f8
honorine1 <uwurukundoh@gmail.com>
2019-08-25 11:40:30 +0200

    highlight directive content  is added

--- Quote/src/app/highlight.directive.ts
+++ Quote/src/app/highlight.directive.ts
@@ -9,25 +9,12 @@ export class HighlightDirective {
   @HostListener("click") upVotes() {
     this.highlight("highlight")
   }
-  @HostListener("dblclick") onDoubleClicks() {
-    this.highlight("highlight")
-  }
+  // @HostListener("dblclick") onDoubleClicks() {
+  //   this.highlight("highlight")
+  // }
   private highlight(highlight: string){
     this.elem.nativeElement.style.backgroundColor = 'yellow';//style will be fill with yellow as bg color on click trigger
     
   }
   }
-// constructor(private elem:ElementRef) { }
-// @HostListener("click") onClicks(){
-//   this.textDeco("line-through")
-// }
-// @HostListener("dbclick") onDoubleClicks(){
-//   this.textDeco("None")
-// }
-// this.elem.nativeElement.style.textDecoration='line-through';
-// private textDeco(action:string){
-// this.elem.nativeElement.style.textDecoration=action;
-// }
-
-// }
 
